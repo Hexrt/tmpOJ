f2cdd749d0ad6fbfcaae0fe055fc870235cc1e28
Hexrt <github@yoosec.com>
2020-07-21 00:40:29 +0800

    Just Test The Server

---
+++ src/main/java/cn/ctrls/tmpoj/TmpojApplication.java
@@ -0,0 +1,12 @@
+package cn.ctrls.tmpoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpojApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TmpojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ctrls/tmpoj/TmpojApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ctrls.tmpoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmpojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
